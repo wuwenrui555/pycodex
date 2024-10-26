d425371184f880829037b06045510ef86e1998b5
wuwenrui555 <wuwenruiwwr@outlook.com>
2024-10-26 01:39:21 -0400

    update io: compatible for data with/without subfolders

--- pycodex/io.py
+++ pycodex/io.py
@@ -34,7 +34,6 @@ def rename_duplicate_markers(marker_list: list[str]) -> list[str]:
     """
     seen = {}
     renamed_list = []
-
     for marker in marker_list:
         if marker in seen:
             seen[marker] += 1
@@ -42,16 +41,17 @@ def rename_duplicate_markers(marker_list: list[str]) -> list[str]:
         else:
             seen[marker] = 1
             renamed_list.append(marker)
+    print(f"Duplicated markers: {[marker for marker, n in seen.items() if n > 1]}")
 
     return renamed_list
 
 
 ########################################################################################################################
-# fusion
+# organize metadata
 ########################################################################################################################
 
 
-def parse_marker_fusion(marker_path: str) -> tuple[str, str, str, str, str]:
+def parse_marker_fusion(marker_path: str) -> pd.DataFrame:
     """
     Fusion: Parse marker information from the file name.
 
@@ -59,63 +59,17 @@ def parse_marker_fusion(marker_path: str) -> tuple[str, str, str, str, str]:
         marker_path (str): Path to the marker image file (*.tif).
 
     Returns:
-        tuple: Parsed marker information including path, region, cycle, channel, and renamed marker.
+        pd.DataFrame: DataFrame of metadata.
     """
     marker_basename = os.path.basename(marker_path)
-    marker_dirname = os.path.dirname(marker_path)
-    region = os.path.basename(marker_dirname)
-    pattern = r"(.+)\.tif"
+    pattern = r"(.+)\.tiff?$"
     match = re.match(pattern, marker_basename)
     (marker,) = match.groups()
-    return marker_path, region, rename_invalid_marker(marker)
+    metadata = pd.DataFrame([[marker_path, rename_invalid_marker(marker)]], columns=["path", "marker"])
+    return metadata
 
 
-def get_marker_metadata_fusion(region_dir: str) -> tuple[str, pd.DataFrame]:
-    """
-    Fusion: Get metadata for all markers in a given region directory.
-
-    Args:
-        region_dir (str): Directory containing marker files for a specific region.
-
-    Returns:
-        tuple: Region name and metadata DataFrame containing paths, region, cycle, channel, and marker.
-    """
-    marker_paths = []
-    for root, dirs, files in os.walk(region_dir):
-        for file in files:
-            marker_paths.append(os.path.join(root, file))
-    metadata_df = pd.DataFrame(
-        [parse_marker_fusion(path) for path in marker_paths],
-        columns=["path", "region", "marker"],
-    )
-    region = metadata_df["region"].unique().item()
-    return region, metadata_df
-
-
-def organize_metadata_fusion(marker_dir: str) -> dict[str, pd.DataFrame]:
-    """
-    Organize metadata for all regions in the final directory.
-
-    Args:
-        marker_dir (str): Directory containing subdirectories which contain marker files for a specific region.
-
-    Returns:
-        dict: Dictionary containing region names as keys and metadata DataFrames as values.
-    """
-    region_dirs = [os.path.join(marker_dir, subdir) for subdir in os.listdir(marker_dir)]
-    metadata_dict = {}
-    for region_dir in region_dirs:
-        region, metadata_df = get_marker_metadata_fusion(region_dir)
-        metadata_dict[region] = metadata_df
-    return metadata_dict
-
-
-########################################################################################################################
-# keyence
-########################################################################################################################
-
-
-def parse_marker_keyence(marker_path: str) -> tuple[str, str, str, str, str]:
+def parse_marker_keyence(marker_path: str) -> pd.DataFrame:
     """
     Keyence: Parse marker information from the file name.
 
@@ -123,50 +77,93 @@ def parse_marker_keyence(marker_path: str) -> tuple[str, str, str, str, str]:
         marker_path (str): Path to the marker image file (*.tif).
 
     Returns:
-        tuple: Parsed marker information including path, region, cycle, channel, and renamed marker.
+        pd.DataFrame: DataFrame of metadata.
     """
     marker_basename = os.path.basename(marker_path)
-    pattern = r"(reg\d+)_(cyc\d+)_(ch\d+)_(.+)\.tif"
+    pattern = r"(reg\d+)_(cyc\d+)_(ch\d+)_(.+)\.tiff?$"
     match = re.match(pattern, marker_basename)
     region, cycle, channel, marker = match.groups()
-    return marker_path, region, cycle, channel, rename_invalid_marker(marker)
-
-
-def get_marker_metadata_keyence(region_dir: str) -> tuple[str, pd.DataFrame]:
-    """
-    Keyence: Get metadata for all markers in a given region directory.
-
-    Args:
-        region_dir (str): Directory containing marker files for a specific region.
-
-    Returns:
-        tuple: Region name and metadata DataFrame containing paths, region, cycle, channel, and marker.
-    """
-    marker_paths = []
-    for root, dirs, files in os.walk(region_dir):
-        for file in files:
-            marker_paths.append(os.path.join(root, file))
-    metadata_df = pd.DataFrame(
-        [parse_marker_keyence(path) for path in marker_paths],
-        columns=["path", "region", "cycle", "channel", "marker"],
+    metadata = pd.DataFrame(
+        [[marker_path, region, cycle, channel, rename_invalid_marker(marker)]],
+        columns=["path", "_region", "cycle", "channel", "marker"],
     )
-    region = metadata_df["region"].unique().item()
-    return region, metadata_df
+    return metadata
 
 
-def organize_metadata_keyence(marker_dir: str) -> dict[str, pd.DataFrame]:
+def _organize_metadata(
+    marker_dir: str, parse_marker_func, subfolders: bool = True, extensions: list[str] = [".tiff", ".tif"]
+):
     """
-    Keyence: Organize metadata for all regions in the final directory.
+    Internal function to organize metadata from marker files.
 
     Args:
-        marker_dir (str): Directory containing subdirectories which contain marker files for a specific region.
+        marker_dir (str): Directory containing marker images or subdirectories of marker images.
+        parse_marker_func (function): Function to parse individual marker image files into DataFrames.
+        subfolders (bool): If True, marker files are in subfolders of marker_dir (e.g., one subfolder per region).
+            If False, marker files are directly in marker_dir. Default is True.
+        extensions (list): List of allowed file extensions. Default is [".tiff", ".tif"].
 
     Returns:
-        dict: Dictionary containing region names as keys and metadata DataFrames as values.
+        dict: Dictionary with region names as keys and metadata DataFrames as values.
     """
-    region_dirs = [os.path.join(marker_dir, subdir) for subdir in os.listdir(marker_dir)]
     metadata_dict = {}
-    for region_dir in region_dirs:
-        region, metadata_df = get_marker_metadata_keyence(region_dir)
+    if subfolders:
+        for region in os.listdir(marker_dir):
+            region_dir = os.path.join(marker_dir, region)
+            metadata_dfs = [
+                parse_marker_func(os.path.join(region_dir, marker))
+                for marker in os.listdir(region_dir)
+                if os.path.splitext(marker)[1] in extensions
+            ]
+            metadata_df = pd.concat(metadata_dfs, axis=0).reset_index(drop=True)
+            metadata_df["region"] = region
+            metadata_dict[region] = metadata_df
+    else:
+        region = "region"
+        metadata_dfs = [
+            parse_marker_func(os.path.join(marker_dir, marker))
+            for marker in os.listdir(marker_dir)
+            if os.path.splitext(marker)[1] in extensions
+        ]
+        metadata_df = pd.concat(metadata_dfs, axis=0).reset_index(drop=True)
+        metadata_df["region"] = region
         metadata_dict[region] = metadata_df
     return metadata_dict
+
+
+def organize_metadata_fusion(
+    marker_dir: str, subfolders: bool = True, extensions: list[str] = [".tiff", ".tif"]
+) -> dict[str, pd.DataFrame]:
+    """
+    Organize metadata from marker files for Fusion output.
+
+    Args:
+        marker_dir (str): Directory containing marker images or subdirectories of marker images.
+        parse_marker_func (function): Function to parse individual marker image files into DataFrames.
+        subfolders (bool): If True, marker files are in subfolders of marker_dir (e.g., one subfolder per region).
+            If False, marker files are directly in marker_dir. Default is True.
+        extensions (list): List of allowed file extensions. Default is [".tiff", ".tif"].
+
+    Returns:
+        dict: Dictionary with region names as keys and metadata DataFrames as values.
+    """
+    return _organize_metadata(marker_dir, parse_marker_fusion, subfolders, extensions)
+
+
+def organize_metadata_keyence(
+    marker_dir: str, subfolders: bool = True, extensions: list[str] = [".tiff", ".tif"]
+) -> dict[str, pd.DataFrame]:
+    """
+    Organize metadata from marker files for Keyence output.
+
+    Args:
+        marker_dir (str): Directory containing marker images or subdirectories of marker images.
+        parse_marker_func (function): Function to parse individual marker image files into DataFrames.
+        subfolders (bool): If True, marker files are in subfolders of marker_dir (e.g., one subfolder per region).
+            If False, marker files are directly in marker_dir. Default is True.
+        extensions (list): List of allowed file extensions. Default is [".tiff", ".tif"].
+
+    Returns:
+        dict: Dictionary with region names as keys and metadata DataFrames as values.
+    """
+    return _organize_metadata(marker_dir, parse_marker_keyence, subfolders, extensions)
